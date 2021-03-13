8f8c3186080f8983f1d11080dd6a33cf5acded11
tosintubi <tosintubi@users.noreply.github.com>
2021-03-13 10:17:22 +0100

    minimumValueAndAverage in single loop

--- src/homework2/IntegerArray.java
+++ src/homework2/IntegerArray.java
@@ -1,22 +1,27 @@
 package homework2;
 
+import java.text.DecimalFormat;
+
 public  class IntegerArray {
 
 
     public static void main(String[] args) {
-        int[] testArray = new int[] { -81,2,3,4,5,6,7,-81,9,10 };
-        System.out.println(MinimumValue(testArray));
+        int[] testArray = new int[] { 1,2,3,4,5,6,7,89,9,10 };
+        minimumValueAndAverage(testArray);
     }
-    public static int MinimumValue(int[] intArray){
-
+    public static void minimumValueAndAverage(int[] intArray){
         int minimum = Integer.MAX_VALUE; // Set to a very high value
+        float sum = 0;
         int index = -1;
+
         for (int i = 0; i < intArray.length-1 ; i++) {
-            minimum = Math.min(intArray[i],minimum);
-            //minimum = intArray[i] < minimum ?  intArray[i] : minimum;
+            minimum = Math.min(intArray[i],minimum); //minimum = intArray[i] < minimum ?  intArray[i] : minimum;
+            sum +=intArray[i];
             index = i;
         }
-        System.out.println(minimum + " is the minimum value and its found at index : "+ index);
-        return  minimum;
+
+        System.out.println("Minimum value  is : "+ minimum + " and it's  at index : "+ index);
+        System.out.println("Average of the array is :" + String.format("%.2f",sum/ intArray.length));
+
     }
 }
