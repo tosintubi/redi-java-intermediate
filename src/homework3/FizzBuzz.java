e363d767ad8f6ad71b9a8d007659cdc81bb979e3
tosintubi <tosintubi@users.noreply.github.com>
2021-03-17 20:29:08 +0100

    indented each condition

--- src/homework3/FizzBuzz.java
+++ src/homework3/FizzBuzz.java
@@ -2,7 +2,13 @@ package homework3;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        for (int  i=1; i < 101; i++){
+        //optimalFizBuzz();
+        FizBuzz();
+    }
+
+
+    public static void FizBuzz(){
+        for (int  i=1; i < 501; i++){
             if ((i % 3 == 0) && (i % 5 ==0))
                 System.out.println("FizzBuzz");
             else if (i % 3 == 0)
@@ -13,4 +19,34 @@ public class FizzBuzz {
                 System.out.println(i);
         }
     }
+
+    public static void optimalFizBuzz(){
+
+        for (int i = 1; i <= 501; ++i)
+        {
+            Boolean fizz = i % 3 == 0;
+            Boolean buzz = i % 5 == 0;
+
+            if (fizz && buzz) // MOD 15
+            {
+                System.out.println("FizzBuzz");
+            }
+            else if (fizz)
+            {
+                System.out.println("Fizz");
+            }
+            else if (buzz)
+            {
+                System.out.println("Buzz");
+            }
+            else
+            {
+                System.out.println(i);
+            }
+        }
+    }
 }
+
+
+
+
