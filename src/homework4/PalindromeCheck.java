20cf38dd3c6504c46fb98aae8fd7febdefa9480c
tosintubi <tosintubi@users.noreply.github.com>
2021-03-22 16:29:51 +0100

    PalindromeChecker recursive function

--- src/homework4/PalindromeCheck.java
+++ src/homework4/PalindromeCheck.java
@@ -1,5 +1,37 @@
 package homework4;
 
+import java.util.Locale;
+
 public class PalindromeCheck {
 
+    public static void main(String[] args) {
+        System.out.println(isPal("Madam"));
+    }
+
+
+    public static boolean isPal(String str)
+    {
+        String s = str.toLowerCase(Locale.ROOT);
+
+        // if length is 0 or 1 then String is palindrome
+
+        if(s.length() == 0 || s.length() == 1)
+            return true;
+        if(s.charAt(0) == s.charAt(s.length()-1)){
+            /* check for first and last char of String:
+             * if they are same then do the same thing for a substring
+             * with first and last char removed. and carry on this
+             * until you string completes or condition fails
+             * Function calling itself: Recursion
+             */
+            return isPal(s.substring(1, s.length()-1));
+        }
+
+        /* If program control reaches to this statement it means
+         * the String is not palindrome hence return false.
+         */
+        return false;
+    }
 }
+
+
