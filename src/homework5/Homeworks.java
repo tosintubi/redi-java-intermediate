42bb248f5077f614ab93ac922b86fa5a1ee6b64e
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-03-28 00:43:14 +0100

    second smallest intArray using sortMethod

--- src/homework5/Homeworks.java
+++ src/homework5/Homeworks.java
@@ -1,12 +1,16 @@
 package homework5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Homeworks {
 
     public static void main(String[] args) {
 
+        int[]  sampleArray = {-1,0,4,3,8,6, -2};
+        smallestSecondElement(sampleArray);
+        secondMost(sampleArray);
     }
 
     /* Write a program to find the  smallest element in an array of integers.  */
@@ -25,5 +29,33 @@ public class Homeworks {
     }
 
 
+    public static void smallestSecondElement(int[] intArray){
+        System.out.println("Original numeric array : "+ Arrays.toString(intArray));
+        int min = Integer.MAX_VALUE;
+        int second_min = Integer.MAX_VALUE;
+        for (int i = 0; i < intArray.length; i++) {
+            if(intArray[i]==min){
+                second_min=min;
+            } else if (intArray[i] < min) {
+                second_min = min;
+                min = intArray[i];
+            } else if (intArray[i] < second_min) {
+                second_min = intArray[i];
+            }
 
+        }
+        System.out.println("Second lowest number is : " + second_min);
+    }
+
+
+    // Simple implementation. Sort the array in desc(or asc, if ) order then pick the next most highest or smallest element
+    public static void secondMost(int[] intArray){
+        /*if (desc != true) // ascending order
+            Arrays.sort(intArray);
+        else
+            Arrays.sort(intArray, Collections.reverseOrder());// descending order*/
+        Arrays.sort(intArray);
+        System.out.println("Second lowest number is : " + intArray[1]);
+
+    }
 }
