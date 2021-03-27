2771ae9276de60169820fb56b1ddfd871de97259
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-03-27 20:03:31 +0100

    remove whitespace in output string

--- src/homework5/MatrixOperations.java
+++ src/homework5/MatrixOperations.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class MatrixOperations {
 
     public static void main(String[] args) {
-        System.out.println(" Sum of the matrix diagonal is : "+matrixDiagonals());
+        System.out.println("Sum of the matrix diagonal is : "+matrixDiagonals());
     }
 
 
