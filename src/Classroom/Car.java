48c876147c0292120caba01e4c04bb164f118d5e
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-03-29 23:50:39 +0200

    Car Class declaration

--- src/Classroom/Car.java
+++ src/Classroom/Car.java
@@ -6,21 +6,36 @@ public class Car {
     private String  color;
     private String type;
     private  int speed;
+    private int gear;
 
-
+    // NoArgs constructor
     public Car() {
         this.color = "Blue";
         this.type = "Mercedes";
         this.speed = 0;
     }
 
+    public Car(String color, String type, int speed) {
+        this.color = color;
+        this.type = type;
+        this.speed = speed;
+    }
 
     public void paint(String newColor){
         this.color = newColor;
     }
 
+    public String getColor(){
+        return this.color;
+    }
+
+
+
     public void accelerate(int newSpeed){
         this.speed = newSpeed;
+        if (speed > 10){
+            this.changeGear(2);
+        }
     }
 
     public String getType()
@@ -32,9 +47,13 @@ public class Car {
         return  this.speed;
     }
 
-    public void printCar(){
+    public void print(){
         System.out.println("Car Type: "+this.type +"\n" +
                 "Color is :"+this.color +"\n"+
                 "Speed is :"+this.speed);
+    }
+
+    private void changeGear(int newGear){
+        this.gear = newGear;
     }
 }
