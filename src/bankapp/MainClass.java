9ca5cdb3f72f7355f3bc7bb6cefd9c17311862c8
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-05-10 21:10:06 +0200

    total Income

--- src/bankapp/MainClass.java
+++ src/bankapp/MainClass.java
@@ -3,10 +3,15 @@ package bankapp;
 public class MainClass {
     public static void main(String[] args) {
         Account tosinAccount = new Account("Tosin ");
+        tosinAccount.addTransaction(new Transaction(250F,"Dec", "2020", TransactionType.INCOME));
         tosinAccount.addTransaction(new Transaction(28F,"Jan", "2021", TransactionType.INCOME));
         tosinAccount.addTransaction(new Transaction(50F,"Feb", "2021", TransactionType.INCOME));
         tosinAccount.addTransaction(new Transaction(-45F,"March", "2021", TransactionType.TRANSPORT));
         tosinAccount.addTransaction(new Transaction(-11.70F,"May", "2021", TransactionType.ENTERTAINMENT));
+        tosinAccount.addTransaction(new Transaction(-11.70F,"May", "2021", TransactionType.ENTERTAINMENT));
+        tosinAccount.addTransaction(new Transaction(-4.70F,"Jan", "2021", TransactionType.ENTERTAINMENT));
+        tosinAccount.addTransaction(new Transaction(-2.70F,"May", "2020", TransactionType.ENTERTAINMENT));
+        tosinAccount.addTransaction(new Transaction(90F,"May", "2021", TransactionType.INCOME));
 
         Transaction maxTx = tosinAccount.maxExpense();
         System.out.println("The biggest payment is:"+ maxTx.getId() + " ["+ maxTx.getAmount() +"$, "+ maxTx.getTransactionType() + ", "+ maxTx.getYear()+ ","+ maxTx.getMonth()+"]");
