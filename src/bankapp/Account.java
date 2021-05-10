792a3d5e01d1ec13b6aac914fb9aa0e1218487e4
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-05-10 20:08:15 +0200

    Add transactions

--- src/bankapp/Account.java
+++ src/bankapp/Account.java
@@ -1,7 +1,18 @@
 package bankapp;
 
+import java.util.ArrayList;
+
 public class Account {
     private String accountHolder;
-    private Transaction[] transactions;
+    private ArrayList<Transaction> transactions;
 
+    public Account(String accountHolder) {
+        this.accountHolder = accountHolder;
+        this.transactions = new ArrayList<>();
+    }
+
+    public int addTransaction(Transaction transaction){
+        transactions.add(transaction);
+        return transactions.size();
+    }
 }
