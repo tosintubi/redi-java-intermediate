9811d4624e798688767058dde7c8dccd3f308992
tosintubi <1065402+tosintubi@users.noreply.github.com>
2021-05-13 23:48:35 +0200

    total Income

--- src/bankapp/MainClass.java
+++ src/bankapp/MainClass.java
@@ -6,9 +6,9 @@ public class MainClass {
         someAccount.addTransaction(new Transaction(250F,"Dec", "2020", TransactionType.INCOME));
         someAccount.addTransaction(new Transaction(28F,"Jan", "2021", TransactionType.INCOME));
         someAccount.addTransaction(new Transaction(50F,"Feb", "2021", TransactionType.INCOME));
-        someAccount.addTransaction(new Transaction(-45F,"March", "2021", TransactionType.TRANSPORT));
+        someAccount.addTransaction(new Transaction(-45F,"March", "2020", TransactionType.TRANSPORT));
         someAccount.addTransaction(new Transaction(-11.70F,"May", "2021", TransactionType.ENTERTAINMENT));
-        someAccount.addTransaction(new Transaction(-11.70F,"May", "2021", TransactionType.ENTERTAINMENT));
+        someAccount.addTransaction(new Transaction(-11.70F,"May", "2019", TransactionType.ENTERTAINMENT));
         someAccount.addTransaction(new Transaction(-4.70F,"Jan", "2021", TransactionType.ENTERTAINMENT));
         someAccount.addTransaction(new Transaction(-2.70F,"May", "2020", TransactionType.ENTERTAINMENT));
         someAccount.addTransaction(new Transaction(90F,"May", "2021", TransactionType.INCOME));
